42da420ef50454dffef50a4fe9ac1957e209716a
Phillipe R Souza <8622005+phillrog@users.noreply.github.com>
2025-12-11 06:52:34 -0300

    bff buscar saldo

--- Src/BankMore.BFF.Web/Controllers/v1/ContasCorrentesController.cs
+++ Src/BankMore.BFF.Web/Controllers/v1/ContasCorrentesController.cs
@@ -1,4 +1,5 @@
 ﻿using BankMore.Domain.Common;
+using BankMore.Domain.Common.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BankMore.BFF.Web.Controllers.V1
@@ -29,7 +30,7 @@ namespace BankMore.BFF.Web.Controllers.V1
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetInformacoes([FromServices] IHttpContextAccessor httpContextAccessor)
+        public async Task<IActionResult> GetInformacoes()
         {
             var claimNumeroConta = User.FindFirst("numero_conta");
 
@@ -41,6 +42,41 @@ namespace BankMore.BFF.Web.Controllers.V1
             string numeroConta = claimNumeroConta.Value;
 
             var response = await _downstreamClient.GetAsync($"/api/v1/ContaCorrente/informacoes?numeroConta={numeroConta}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+
+            return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+        }
+
+        /// <summary>
+        /// Consulta o saldo atual e os totais de crédito/débito de uma conta corrente.
+        /// </summary>
+        /// <remarks>
+        /// Permite a consulta do próprio saldo ou o saldo de terceiros (para Masters/Admins).
+        /// </remarks>                
+        /// <returns>Retorna um SaldoDto contendo o saldo atualizado e os totais de crédito/débito, ou um erro.</returns>
+        [HttpGet("saldo")]
+        [ProducesResponseType(typeof(InformacoesContaCorrenteDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetSaldo()
+        {
+            var claimNumeroConta = User.FindFirst("numero_conta");
+
+            if (claimNumeroConta == null)
+            {
+                return Unauthorized("A claim 'numero_conta' não foi encontrada.");
+            }
+
+            string numeroConta = claimNumeroConta.Value;
+
+            var response = await _downstreamClient.GetAsync($"/api/v1/ContaCorrente/saldo?numeroConta={numeroConta}");
 
             if (response.IsSuccessStatusCode)
             {
